b9a90fc2fadecb10a7a43759d2596a8809107578
Sohel <jmsohelrana04@gmail.com>
2023-06-19 15:57:01 +0600

    some scss file added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import Header from "./components/Header";
+import "./styles/app.scss";
+import "./styles/colors.scss";
 function App() {
   return (
     <BrowserRouter>
+      <Header></Header>
       <Routes>
         <Route></Route>
       </Routes>
